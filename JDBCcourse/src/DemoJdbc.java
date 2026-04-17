0448d6031fc1c0359ca1d329ea505897897f165c
Kashi-R <routraykashinatha@gmail.com>
2026-04-17 08:47:18 +0530

    problems in statement

--- JDBCcourse/src/DemoJdbc.java
+++ JDBCcourse/src/DemoJdbc.java
@@ -12,15 +12,18 @@ public class DemoJdbc {
         process the results
         close
          */
+            int sid = 102;
+            String name = "Max";
+            int marks = 51;
             String url="jdbc:postgresql://localhost:5432/demo" ;
             String uname="postgres";
             String pass="Kashi@321";
-            String sql="update student set name = 'Max' where sid =5";
+            String sql="insert into student values (?,?,?)";
             Class.forName("org.postgresql.Driver");
 
         Connection con = DriverManager.getConnection(url,uname,pass);
         System.out.println("Connection Established");
-        Statement st=con.createStatement();
+        PreStatement st=con.createStatement();
         boolean status =st.execute(sql);
         System.out.println(status);// This gives false if the query
         //is updation type insert, updt, dlt, AND true for rs type from table show etc
